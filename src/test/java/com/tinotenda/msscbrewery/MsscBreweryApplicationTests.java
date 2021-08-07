626d04e9b6d7659b542fb135409029c25da67db1
Tino <Tinotenda.Kanyandura@standardbank.co.za>
2021-08-07 06:33:30 +0200

    Initial commit.

---
+++ src/test/java/com/tinotenda/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinotenda.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
